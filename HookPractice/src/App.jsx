698c5c3c87d99f655cb6a386d4bdb309fb0e8771
Milan Kacha <kachamilan108@gmail.com>
2023-09-12 16:41:40 +0530

    useRefHook with focus input

--- HookPractice/src/App.jsx
+++ HookPractice/src/App.jsx
@@ -1,9 +1,28 @@
-import UseCallBackHook from "./useCallbackHook/UseCallBackHook";
+import { useRef, useState } from "react";
 
 function App() {
+  const refElement = useRef("");
+  const [name, setName] = useState("");
+
+  function Reset() {
+    setName("");
+    refElement.current.focus();
+  }
+  function Color() {
+    refElement.current.style.color = "red";
+  }
+  // console.log(refElement);
   return (
     <>
-      <UseCallBackHook />
+      <h1>Learing UseRef</h1>
+      <input
+        type="text"
+        value={name}
+        ref={refElement}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={Reset}>Reset</button>
+      <button onClick={Color}>Change Text color</button>
     </>
   );
 }
